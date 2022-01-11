b84eec7ec1eda509e1f0ba025692e1a1f99767fd
tariq <tariqshariff.ts@gmail.com>
2022-01-11 12:12:06 +0530

    climb stairs - dp

---
+++ Sheet/Dynamic Programming/2_climb_stairs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n)
+{
+    if (n == 1)
+        return 1;
+    vector<int> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
