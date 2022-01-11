f3cedabc3c69e4fa2ad3127a56efddaf3d72dc35
tariq <tariqshariff.ts@gmail.com>
2022-01-11 12:06:05 +0530

    max subarray sum - dp

---
+++ Sheet/Dynamic Programming/1_maximum_subarray.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> dp(n);
+    dp[0] = nums[0];
+    int max_sum = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+        max_sum = max(max_sum, dp[i]);
+    }
+    return max_sum;
+}
