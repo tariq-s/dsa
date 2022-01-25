fa05a272cc0f7e75681d17b627c5b5dae33de754
tariq <tariqshariff.ts@gmail.com>
2022-01-25 18:57:49 +0530

    btree max path sum - trees

---
+++ Sheet/Trees/28_btree_max_path_sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int ans = INT_MIN;
+int max_path(TreeNode *root)
+{
+    if (!root)
+        return INT_MIN;
+
+    int left = max_path(root->left);
+    int right = max_path(root->right);
+
+    left = left > 0 ? left : 0;
+    right = right > 0 ? right : 0;
+
+    ans = max(ans, root->val + left + right);
+    return root->val + max(left, right);
+}
+int maxPathSum(TreeNode *root)
+{
+    max_path(root);
+    return ans;
+}
