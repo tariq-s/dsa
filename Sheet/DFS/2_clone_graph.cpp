963a2c0c5983ad729ccc5b8ccbb7c19e9ffa92c5
tariq <tariqshariff.ts@gmail.com>
2022-02-08 19:53:59 +0530

    clone graph - dfs

--- Sheet/DFS/2_clone_graph.cpp
+++ Sheet/DFS/2_clone_graph.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
 Node *clone_dfs(vector<bool> &visited, Node *node, unordered_map<Node *, Node *> &map)
 {
     if (!node)
